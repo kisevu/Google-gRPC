ea63ce7e42c8d681c70659b06dc18d1b4aa6ae8d
ameda-kevin <amedakevin@gmail.com>
2024-08-06 14:29:13 +0300

    server setup

--- src/main/java/com/ameda/kisevu/Main.java
+++ src/main/java/com/ameda/kisevu/Main.java
@@ -1,9 +1,27 @@
 package com.ameda.kisevu;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Dear ameda Kevin you are going places kido");
+    public static void main(String[] args) throws IOException,
+            InterruptedException {
+        System.out.println("Hello gRPC");
+        Server server = ServerBuilder
+                .forPort(50051)
+                .build();
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread( ()->{
+            System.out.println("Received shutdown request.");
+            server.shutdown();
+            System.out.println("Successfully stopped the server.");
+        } ));
+
+        server.awaitTermination();
     }
 }
